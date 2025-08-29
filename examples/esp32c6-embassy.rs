a3b0a3a61cb8c845e0131ba9d07b9904513b8f13
Paul Bender <pebender@gmail.com>
2025-08-28 20:57:46 -0700

    Change example so  touch and LCD are in separate tasks ancd communicate using a channel.

--- examples/esp32c6-embassy.rs
+++ examples/esp32c6-embassy.rs
@@ -11,8 +11,12 @@ use esp_println as _;
 
 use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
 use embassy_executor::Spawner;
-use embassy_sync::{blocking_mutex, blocking_mutex::raw::CriticalSectionRawMutex};
-use embassy_time::Delay;
+use embassy_sync::{
+    blocking_mutex,
+    blocking_mutex::raw::CriticalSectionRawMutex,
+    channel::{Channel, Receiver, Sender},
+};
+use embassy_time::{Delay, Timer};
 
 use embedded_graphics::prelude::*;
 
@@ -33,6 +37,28 @@ struct PeripheralMap<'a> {
     pub lcd_backlight: GPIO_LCD_BACKLIGHT<'a>,
     pub touch_cs: GPIO_TOUCH_CS<'a>,
     pub touch_irq: GPIO_TOUCH_IRQ<'a>,
+}
+
+enum LcdCommand {
+    Clear,
+    TouchPoint(embedded_graphics::geometry::Point),
+}
+
+// The embassy-executor main sets up shared hardware resources, resulting in
+// structs that represent the hardware. main shares references to these structs
+// with embassy-executor tasks that make use of these hardware resources.
+// Because main spawns these tasks, the Rust compiler cannot infer when the
+// tasks will compete. As a result, it cannot infer whether or not the structs
+// representing the hardware will last the lifetime of the spawned task. To
+// solve this, main makes these structs static using StaticCell. For those not
+// familiar with StaticCell, it allows you to statically allocate data that
+// requires runtime initialization. This macro takes the data's type (time) and
+// value (v) and returns a static reference to the data.
+macro_rules! make_static {
+    ($time:ty,$v:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$time> = static_cell::StaticCell::new();
+        STATIC_CELL.init(($v))
+    }};
 }
 
 pub struct MyIrq<'a>(esp_hal::gpio::Input<'a>);
@@ -86,30 +112,133 @@ async fn main(spawner: Spawner) -> ! {
         .with_mosi(peripheral_map.spi_mosi);
     let spi_bus =
         blocking_mutex::Mutex::<CriticalSectionRawMutex, _>::new(core::cell::RefCell::new(spi));
+    let spi_bus = &*make_static!(blocking_mutex::Mutex<CriticalSectionRawMutex, core::cell::RefCell<esp_hal::spi::master::Spi<'static, esp_hal::Blocking>>>, spi_bus);
 
+    // Set up the channel for sending commands to the lcd task.
+    let lcd_command_channel = Channel::<CriticalSectionRawMutex, LcdCommand, 16>::new();
+    let lcd_command_channel =
+        &*make_static!(Channel<CriticalSectionRawMutex, LcdCommand, 16>, lcd_command_channel);
+
+    // Create a channel receiver to give to the lcd task.
+    let lcd_command_receiver = lcd_command_channel.receiver();
+    let lcd_command_receiver =
+        &*make_static!(Receiver<CriticalSectionRawMutex, LcdCommand, 16>, lcd_command_receiver);
+
+    // Create a channel sender to give to the touch task.
+    let lcd_command_sender = lcd_command_channel.sender();
+    let lcd_command_sender =
+        &*make_static!(Sender<CriticalSectionRawMutex, LcdCommand, 16>, lcd_command_sender);
+
+    spawner.must_spawn(lcd_task(
+        spi_bus,
+        peripheral_map.lcd_cs,
+        peripheral_map.lcd_dc,
+        peripheral_map.lcd_reset,
+        peripheral_map.lcd_backlight,
+        lcd_command_receiver,
+    ));
+
+    spawner.must_spawn(touch_task(
+        spi_bus,
+        peripheral_map.touch_cs,
+        peripheral_map.touch_irq,
+        lcd_command_sender,
+    ));
+
+    lcd_command_sender.send(LcdCommand::Clear).await;
+
+    loop {
+        Timer::after_secs(60).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn touch_task(
+    spi_bus: &'static blocking_mutex::Mutex<
+        CriticalSectionRawMutex,
+        core::cell::RefCell<esp_hal::spi::master::Spi<'static, esp_hal::Blocking>>,
+    >,
+    touch_cs: GPIO_TOUCH_CS<'static>,
+    touch_irq: GPIO_TOUCH_IRQ<'static>,
+    lcd_command_sender: &'static Sender<'static, CriticalSectionRawMutex, LcdCommand, 16>,
+) -> ! {
+    // Set up the touch SPI device.
+    let touch_cs = esp_hal::gpio::Output::new(
+        touch_cs,
+        esp_hal::gpio::Level::High,
+        esp_hal::gpio::OutputConfig::default(),
+    );
+    let touch_spi_config = esp_hal::spi::master::Config::default()
+        .with_mode(esp_hal::spi::Mode::_0)
+        .with_frequency(esp_hal::time::Rate::from_mhz(2));
+    let touch_spi_device = SpiDeviceWithConfig::new(spi_bus, touch_cs, touch_spi_config);
+
+    let touch_irq: esp_hal::gpio::Input<'_> = esp_hal::gpio::Input::new(
+        touch_irq,
+        esp_hal::gpio::InputConfig::default().with_pull(esp_hal::gpio::Pull::Up),
+    );
+
+    let mut touch = xpt2046::Xpt2046::new(
+        touch_spi_device,
+        MyIrq(touch_irq),
+        xpt2046::Orientation::Portrait,
+    );
+
+    let mut delay = Delay;
+    let mut exti = true;
+
+    touch.init(&mut delay).unwrap();
+    touch.clear_touch();
+    loop {
+        touch.irq.0.wait_for_low().await;
+        while touch.irq.0.is_low() {
+            touch.run(&mut exti).unwrap();
+            if touch.is_touched() {
+                let point = touch.get_touch_point();
+                if point.x >= 0 && point.x < 240 && point.y >= 0 && point.y < 320 {
+                    lcd_command_sender.send(LcdCommand::TouchPoint(point)).await;
+                }
+            }
+            touch.clear_touch();
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn lcd_task(
+    spi_bus: &'static blocking_mutex::Mutex<
+        CriticalSectionRawMutex,
+        core::cell::RefCell<esp_hal::spi::master::Spi<'static, esp_hal::Blocking>>,
+    >,
+    lcd_cs: GPIO_LCD_CS<'static>,
+    lcd_dc: GPIO_LCD_DC<'static>,
+    lcd_reset: GPIO_LCD_RESET<'static>,
+    lcd_backlight: GPIO_LCD_BACKLIGHT<'static>,
+    lcd_command_receiver: &'static Receiver<'static, CriticalSectionRawMutex, LcdCommand, 16>,
+) -> ! {
     // Set up the LCD SPI device.
     let lcd_cs = esp_hal::gpio::Output::new(
-        peripheral_map.lcd_cs,
+        lcd_cs,
         esp_hal::gpio::Level::High,
         esp_hal::gpio::OutputConfig::default(),
     );
     let lcd_spi_config = esp_hal::spi::master::Config::default()
         .with_mode(esp_hal::spi::Mode::_0)
         .with_frequency(esp_hal::time::Rate::from_mhz(40));
-    let lcd_spi_device = SpiDeviceWithConfig::new(&spi_bus, lcd_cs, lcd_spi_config);
+    let lcd_spi_device = SpiDeviceWithConfig::new(spi_bus, lcd_cs, lcd_spi_config);
 
     let lcd_dc = esp_hal::gpio::Output::new(
-        peripheral_map.lcd_dc,
+        lcd_dc,
         esp_hal::gpio::Level::Low,
         esp_hal::gpio::OutputConfig::default(),
     );
     let lcd_reset = esp_hal::gpio::Output::new(
-        peripheral_map.lcd_reset,
+        lcd_reset,
         esp_hal::gpio::Level::Low,
         esp_hal::gpio::OutputConfig::default(),
     );
     let mut lcd_backlight = esp_hal::gpio::Output::new(
-        peripheral_map.lcd_backlight,
+        lcd_backlight,
         esp_hal::gpio::Level::Low,
         esp_hal::gpio::OutputConfig::default(),
     );
@@ -126,35 +255,6 @@ async fn main(spawner: Spawner) -> ! {
         .unwrap();
     lcd_backlight.set_high();
 
-    // Set up the touch SPI device.
-    let touch_cs = esp_hal::gpio::Output::new(
-        peripheral_map.touch_cs,
-        esp_hal::gpio::Level::High,
-        esp_hal::gpio::OutputConfig::default(),
-    );
-    let touch_spi_config = esp_hal::spi::master::Config::default()
-        .with_mode(esp_hal::spi::Mode::_0)
-        .with_frequency(esp_hal::time::Rate::from_mhz(2));
-    let touch_spi_device = SpiDeviceWithConfig::new(&spi_bus, touch_cs, touch_spi_config);
-
-    let touch_irq: esp_hal::gpio::Input<'_> = esp_hal::gpio::Input::new(
-        peripheral_map.touch_irq,
-        esp_hal::gpio::InputConfig::default().with_pull(esp_hal::gpio::Pull::Up),
-    );
-
-    let mut touch = xpt2046::Xpt2046::new(
-        touch_spi_device,
-        MyIrq(touch_irq),
-        xpt2046::Orientation::Portrait,
-    );
-
-    let _ = spawner;
-
-    lcd.clear(embedded_graphics::pixelcolor::Rgb565::CSS_BLACK)
-        .unwrap();
-    let mut delay = Delay;
-    touch.init(&mut delay).unwrap();
-    touch.clear_touch();
     let dot = embedded_graphics::primitives::Rectangle::new(
         embedded_graphics::geometry::Point::zero(),
         embedded_graphics::geometry::Size::new_equal(1),
@@ -162,18 +262,13 @@ async fn main(spawner: Spawner) -> ! {
     .into_styled(embedded_graphics::primitives::PrimitiveStyle::with_fill(
         embedded_graphics::pixelcolor::Rgb565::CSS_WHITE,
     ));
-    let mut exti = true;
+
     loop {
-        touch.irq.0.wait_for_low().await;
-        while touch.irq.0.is_low() {
-            touch.run(&mut exti).unwrap();
-            if touch.is_touched() {
-                let point = touch.get_touch_point();
-                if point.x >= 0 && point.x < 240 && point.y >= 0 && point.y < 320 {
-                    dot.translate(point).draw(&mut lcd).unwrap();
-                }
-            }
-            touch.clear_touch();
+        match lcd_command_receiver.receive().await {
+            LcdCommand::Clear => lcd
+                .clear(embedded_graphics::pixelcolor::Rgb565::CSS_BLACK)
+                .unwrap(),
+            LcdCommand::TouchPoint(p) => dot.translate(p).draw(&mut lcd).unwrap(),
         }
     }
 }
